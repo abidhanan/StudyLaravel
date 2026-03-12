73335e20db035635b34f166bbe7013ed5e2346bd
Abid Hanan <abidhanan0904@gmail.com>
2026-03-12 11:56:27 +0700

    Make TesterController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AdminController\TesterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', [TesterController::class, 'index']);
